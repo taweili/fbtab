e5a55bf98b01f86a13bbb836a32343710d0d6aea
David Li <taweili@yahoo.com>
2011-02-01 10:36:28 +0800

    add skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pagetab::Application.config.secret_token = 'ae732ef745fdf551bef90aef77f723314e696b74eb8d055d847e516619a0bdee5690e5370ba591dd1d676429a7128ff3aa7ce0fa01511d77783746d94b592b1e'
