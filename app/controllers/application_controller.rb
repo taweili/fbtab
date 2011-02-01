089dbbbacf00dc155c703b043ee9b470c92ffd55
David Li <taweili@yahoo.com>
2011-02-01 18:38:17 +0800

    update with current_facebook_page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include Facebooker2::Rails::Controller
+  helper_method :current_facebook_page
   
+  def current_facebook_page
+    if facebook_params[:profile_id]
+      @_current_facebook_page = Mogli::Page.new(:id => facebook_params[:profile_id].to_i)
+    else
+      nil
+    end
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,9 +5,17 @@ class HomeController < ApplicationController
   end
 
   def tab
+    debug_session
   end
 
   def fbml
+    debug_session
+  end
+
+  def debug_session
+    logger.debug("---tab ----- #{current_facebook_user.inspect}")
+    logger.debug("---page----- #{current_facebook_page}")
+    logger.debug("---params--- #{facebook_params.inspect}")
   end
   
 end
